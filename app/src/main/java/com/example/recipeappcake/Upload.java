8c8b94f30ef5c31279ddda9bf7387555ebd10f6e
Emad Ahmed <55825414+Emad-ahmed@users.noreply.github.com>
2021-10-25 10:47:40 -0700

    a

--- app/src/main/java/com/example/recipeappcake/Upload.java
+++ app/src/main/java/com/example/recipeappcake/Upload.java
@@ -1,11 +1,13 @@
 package com.example.recipeappcake;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -15,10 +17,23 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.recipeappcake.Model.Recipe;
+import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
 public class Upload extends AppCompatActivity {
     Uri uri;
     EditText edName, edDesc, edPrice;
-
+    String imageUrl;
     ImageView  imageview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,5 +76,57 @@ public class Upload extends AppCompatActivity {
     }
 
     public void uploadRecipe(View view) {
+        uploadimage();
+    }
+
+    public void uploadimage()
+    {
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading Image Is Uploading");
+        progressDialog.show();
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("RecipeBook").child(uri.getLastPathSegment());
+
+
+        storageReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Task task = taskSnapshot.getStorage().getDownloadUrl();
+                while (!task.isSuccessful());
+                Uri uriimage = (Uri) task.getResult();
+                imageUrl = uriimage.toString();
+                uploadrecipe();
+                Toast.makeText(Upload.this, "Image Uploaded Successfully", Toast.LENGTH_SHORT).show();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                Toast.makeText(Upload.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+        progressDialog.dismiss();
+    }
+
+    public void uploadrecipe(){
+        String name = edName.toString().trim();
+        String description = edDesc.toString().trim();
+        String price = edPrice.getText().toString().trim();
+        String timeStamp = DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
+        Recipe recipe = new Recipe(name, description, price, imageUrl);
+
+        FirebaseDatabase.getInstance().getReference("RecipeBook").child(timeStamp)
+                .setValue(recipe).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Toast.makeText(Upload.this, "Sucess", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Upload.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
